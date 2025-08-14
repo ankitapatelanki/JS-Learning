b80da2517e171c490f4a8131d949b77b8ce65819
Ankita Patel <ankitapatel11068@gmail.com>
2025-08-15 01:37:18 +0530

    taskfile 14-aug(set)

---
+++ Task/14-aug(set).js
@@ -0,0 +1,82 @@
+// JavaScript - 20:  JavaScript Set Practice Assignment  🤞🏻🔥
+// Please don't copy paste answer from any where and don't see the solution from the internet.  Best of Luck 👍🏻
+// You can read the article, but please close it before submitting the form and answer based on your own understanding.
+
+// Q1. What is a Set in JavaScript?
+//                                       /
+// A collection of unique values       \/
+// An array with duplicate elements
+// An object with key-value pairs
+// A loop statement
+// Q2. Which of the following creates an empty Set?
+// *
+// 2 points
+// let s = [];
+// let s = new Set();       \/
+// let s = {};
+// let s = Set();
+// Q3. What will be the output?
+// let s = new Set([1,2,2,3]);
+// console.log(s.size);
+// *
+// 2 points
+// 3                    \/
+// 4
+// 2
+// Error
+// Q4. Which method is used to add a value to a Set?
+// *
+// 2 points
+// push()
+// add()                    \/
+// append()
+// insert()
+// Q5. What will be the output?
+// let s = new Set();
+// s.add('a');
+// s.add('a');
+// console.log(s.size);
+// *
+// 2 points
+// 2
+// 1            \/
+// 0
+// Error
+// Q6. Which method removes a value from a Set?
+// *
+// 2 points
+// remove()
+// delete()                     \/
+// pop()
+// splice()
+// Q7. Output?
+// let s = new Set([1,2,3]);
+// console.log(s.has(2));
+// *
+// 2 points
+// true                         \/
+// false
+// undefined
+// Error
+// Q8. Which loop can be used to iterate over a Set?
+// *
+// 2 points
+// for of                           \/
+// for in
+// while
+// map()
+// Q9. What will be the output?
+// let s = new Set(['x','y']);
+// s.clear();
+// console.log(s.size);
+// *
+// 2 points
+// 0                        \/
+// 1
+// 2
+// Error
+// Q10. Sets maintain insertion order in JavaScript. True or False?
+// *
+// 2 points
+// True                        \/
+// False
